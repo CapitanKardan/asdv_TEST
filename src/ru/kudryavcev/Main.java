50975cb7972731cf7869667246834b7bfc2bb4de
Comrade <mikel20220@gmail.com>
2021-09-30 19:42:50 +0300

    add new sout (color and ggp)

--- src/ru/kudryavcev/Main.java
+++ src/ru/kudryavcev/Main.java
@@ -3,7 +3,8 @@ package ru.kudryavcev;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("vavanv");
+        System.out.println("yellow pink grey");
+        System.out.println("GGP");
     }
 
 }
