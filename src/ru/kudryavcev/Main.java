16cf74ef4b741bff278c86e6d442f8e71a3a77af
Comrade <mikel20220@gmail.com>
2021-09-30 19:55:20 +0300

    test save

--- src/ru/kudryavcev/Main.java
+++ src/ru/kudryavcev/Main.java
@@ -3,7 +3,9 @@ package ru.kudryavcev;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TEST CHECKOUT");
+        System.out.println("yellow pink grey");
+        // TODO: 30.09.2021 try new in this field of proffesstion
+        System.out.println("GGP");
     }
 
 }
